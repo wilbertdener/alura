4ea24ed79733830c462535fa11e76666e8eae87b
wilbert <denerwilbert@gmail.com>
2026-03-22 00:49:27 -0300

    url dinamicas

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -23,8 +23,8 @@ def index():
 @app.route('/novo')
 def novo():
     if 'usuario_logado' not in session or session['usuario_logado']==None:
-        return redirect('/login?proxima=novo')#informação da query string no url
-
+        #return redirect('/login?proxima=novo')#informação da query string no url
+        return redirect(url_for('login', proxima=url_for('novo')))
     return render_template('novo.html', titulo='Novo Jogo')
 
 @app.route('/criar', methods=['POST',])
@@ -35,7 +35,7 @@ def criar():
     console = request.form['console']
     jogo = Jogo(nome,categoria,console)
     lista.append(jogo)
-    return redirect('/')
+    return redirect(url_for('index'))#substitui a rota pela função que instacia
 
 @app.route('/login')
 def login():
@@ -48,16 +48,16 @@ def autenticar():
         session['usuario_logado'] = request.form['usuario']
         flash(session['usuario_logado']  + ' logado com sucesso!')
         proxima_pagina = request.form['proxima']
-        return redirect('/{}'.format(proxima_pagina))
+        return redirect(proxima_pagina)
     else:
         flash('Usuário não logado.')
-        return redirect('/login')
+        return redirect(url_for('login'))
 
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
-    return redirect('/')
+    return redirect(url_for('index'))
 
 
 app.run(debug=True)
