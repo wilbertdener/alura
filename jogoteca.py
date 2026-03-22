3824ca3ee719540ddc084d1598c792e0e01b9b0b
wilbert <denerwilbert@gmail.com>
2026-03-21 23:26:54 -0300

    Criando a tela de login - autentificar

--- jogoteca.py
+++ jogoteca.py
@@ -32,4 +32,15 @@ def criar():
     lista.append(jogo)
     return redirect('/')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    if 'alohomora'== request.form['senha']:
+        return redirect('/')
+    else:
+        return redirect('/login')
+
 app.run(debug=True)
