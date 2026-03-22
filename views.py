38747a6ca73698256427d08f26481a1e970ee4b7
wilbert <denerwilbert@gmail.com>
2026-03-22 20:01:19 -0300

    validação dos campos do form preenchidos e token de segurança

--- views.py
+++ views.py
@@ -1,7 +1,7 @@
 from flask import  render_template, request, redirect, session, flash, url_for, send_from_directory
 from jogoteca import app, db
 from models import Jogos, Usuarios
-from helpers import recupera_imagem, deleta_arquivo
+from helpers import recupera_imagem, deleta_arquivo, FormularioJogo
 import time
 
 @app.route('/')
@@ -15,14 +15,20 @@ def novo():
     if 'usuario_logado' not in session or session['usuario_logado']==None:
         #return redirect('/login?proxima=novo')#informação da query string no url
         return redirect(url_for('login', proxima=url_for('novo')))
-    return render_template('novo.html', titulo='Novo Jogo')
+
+    form=FormularioJogo()
+    return render_template('novo.html', titulo='Novo Jogo', form=form)
 
 @app.route('/criar', methods=['POST',])
 def criar():
+
+    form = FormularioJogo(request.form)
+    if not form.validate_on_submit():
+        return redirect(url_for('novo'))
     #tag name
-    nome = request.form['nome']
-    categoria = request.form['categoria']
-    console = request.form['console']
+    nome = form.nome.data
+    categoria = form.categoria.data
+    console = form.console.data
 
     jogo = Jogos.query.filter_by(nome=nome).first()
     if jogo:
@@ -48,26 +54,32 @@ def editar(id):
         #return redirect('/login?proxima=novo')#informação da query string no url
         return redirect(url_for('login', proxima=url_for('editar')))
     jogo = Jogos.query.filter_by(id=id).first()
-
+    form=FormularioJogo()
+    form.nome.data = jogo.nome
+    form.categoria.data = jogo.categoria
+    form.console.data = jogo.console
     capa_jogo = recupera_imagem(id)
 
-    return render_template('editar.html', titulo='Editando Jogo', jogo=jogo, capa_jogo=capa_jogo)
+    return render_template('editar.html', titulo='Editando Jogo', id=id, capa_jogo=capa_jogo, form=form)
 
 @app.route('/atualizar', methods=['POST',])
 def atualizar():
-    jogo = Jogos.query.filter_by(id=request.form['id']).first()
-    jogo.nome = request.form['nome']
-    jogo.categoria = request.form['categoria']
-    jogo.console = request.form['console']
+    form = FormularioJogo(request.form)
+    if form.validate_on_submit():
 
-    db.session.add(jogo)
-    db.session.commit()
+        jogo = Jogos.query.filter_by(id=request.form['id']).first()
+        jogo.nome = form.nome.data
+        jogo.categoria = form.categoria.data
+        jogo.console = form.console.data
 
-    arquivo = request.files['arquivo']
-    upload_path = app.config['UPLOAD_PATH']
-    timestamp = time.time()
-    deleta_arquivo(jogo.id)
-    arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')#faz referenci ao tempo de agora
+        db.session.add(jogo)
+        db.session.commit()
+
+        arquivo = request.files['arquivo']
+        upload_path = app.config['UPLOAD_PATH']
+        timestamp = time.time()
+        deleta_arquivo(jogo.id)
+        arquivo.save(f'{upload_path}/capa{jogo.id}-{timestamp}.jpg')#faz referenci ao tempo de agora
 
     return redirect(url_for('index'))
 
