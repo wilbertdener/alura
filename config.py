120a3dc83f6aecd48d1e115daa3860c746cd1c0b
wilbert <denerwilbert@gmail.com>
2026-03-22 16:20:58 -0300

    refatoração do codigo, criação dos arquivos config,views,models

---
+++ config.py
@@ -0,0 +1,15 @@
+
+SECRET_KEY = 'alura'  # chave secreta para usar o session
+# trecho da app
+# app.run(host='0.0.0.0', port=8080)
+
+SQLALCHEMY_DATABASE_URI = \
+    '{SGBD}://{usuario}:{senha}@{servidor}/{database}'.format(
+
+        SGBD='mysql+mysqlconnector',
+        usuario='root',
+        senha='',
+        servidor='localhost',
+        database='jogoteca'
+
+    )
