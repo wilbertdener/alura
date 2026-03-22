ecf678361839e3d545386a98e58c395e0016558d
wilbert <denerwilbert@gmail.com>
2026-03-21 23:46:47 -0300

    Deslogar da sessão

--- jogoteca.py
+++ jogoteca.py
@@ -48,4 +48,11 @@ def autenticar():
         flash('Usuário não logado.')
         return redirect('/login')
 
+@app.route('/logout')
+def logout():
+    session['usuario_logado'] = None
+    flash('Logout efetuado com sucesso!')
+    return redirect('/')
+
+
 app.run(debug=True)
